a410ac0775412da55c6d91632c1e2e9519102880
MirT3050D <rahajarijaonamir@gmail.com>
2025-07-04 08:54:01 +0300

    chnage

--- src/main/java/com/controller/AccueilController.java
+++ src/main/java/com/controller/AccueilController.java
@@ -4,6 +4,7 @@ import org.springframework.stereotype.Controller;
 import com.entity.Adherent;
 import com.service.AdherentService;
 
+import jakarta.servlet.http.HttpSession;
 
 import org.springframework.web.bind.annotation.*;
 
@@ -11,13 +12,17 @@ import java.util.List;
 
 @Controller
 public class AccueilController {
+<<<<<<< Updated upstream
     @GetMapping("/login")
     public String goLogin()
     {
         return "login";
     }
+=======
+>>>>>>> Stashed changes
     @GetMapping("/")
     public String accueil() {
+
         return "dashboard";
     }
 }
